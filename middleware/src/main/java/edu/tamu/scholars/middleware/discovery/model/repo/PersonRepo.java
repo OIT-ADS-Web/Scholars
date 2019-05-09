638bd443502b28ddfe8d7d3b87b366c8c610838c
Jim Wood <jim.wood@duke.edu>
2019-05-09 12:13:01 -0400

    Spike on GraphQL support
    Add graphql and graphiql dependencies
    
    Test schema definition and resolver ideas via listing people and
    resolving the first name field

--- middleware/src/main/java/edu/tamu/scholars/middleware/discovery/model/repo/PersonRepo.java
+++ middleware/src/main/java/edu/tamu/scholars/middleware/discovery/model/repo/PersonRepo.java
@@ -4,7 +4,12 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import edu.tamu.scholars.middleware.discovery.model.Person;
 
+import java.util.List;
+
 @RepositoryRestResource
 public interface PersonRepo extends SolrDocumentRepo<Person> {
+
+  // throw away method - testing as a proof of concept
+  List<Person> findByFirstName(String firstName);
 
 }
---
+++ middleware/src/main/java/edu/tamu/scholars/middleware/graphql/resolver/PersonQuery.java
@@ -0,0 +1,25 @@
+package edu.tamu.scholars.middleware.graphql.resolver;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import edu.tamu.scholars.middleware.discovery.model.Person;
+import edu.tamu.scholars.middleware.discovery.model.repo.PersonRepo;
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import java.util.List;
+
+@Component
+public class PersonQuery implements GraphQLQueryResolver {
+
+  @Autowired
+  private PersonRepo personRepo;
+
+  public List<Person> listPersons() {
+    return personRepo.findByType("person");
+  }
+
+  public List<Person> findByFirstName(String firstName) {
+    return personRepo.findByFirstName(firstName);
+  }
+}
+
