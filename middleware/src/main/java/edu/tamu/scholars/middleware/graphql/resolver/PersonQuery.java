c988c4191cb51b02c634d39bacd3cb74ea9a1d38
Jim Wood <jim.wood@duke.edu>
2019-05-14 10:43:43 -0400

    Add paging support and document resolver
    Resolve documents for a person - need to find by name for now since ids
    are embedded in a serialized fields

--- middleware/src/main/java/edu/tamu/scholars/middleware/graphql/resolver/PersonQuery.java
+++ middleware/src/main/java/edu/tamu/scholars/middleware/graphql/resolver/PersonQuery.java
@@ -1,15 +1,18 @@
 package edu.tamu.scholars.middleware.graphql.resolver;
 
+import java.util.List;
+
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 
 import edu.tamu.scholars.middleware.discovery.model.Person;
 import edu.tamu.scholars.middleware.discovery.model.repo.PersonRepo;
-import com.coxautodev.graphql.tools.GraphQLQueryResolver;
-import java.util.List;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.PageRequest;
+import edu.tamu.scholars.middleware.graphql.inputmodel.Paging;
 
 @Component
 public class PersonQuery implements GraphQLQueryResolver {
@@ -17,9 +20,16 @@ public class PersonQuery implements GraphQLQueryResolver {
   @Autowired
   private PersonRepo personRepo;
 
-  public List<Person> searchPeople(String query) {
-    Pageable pageable = PageRequest.of(0,10);
-    Page<Person> personPage = personRepo.findByCustomQuery(query, pageable);
+  public List<Person> people(String query, Paging paging) {
+
+    Page<Person> personPage;
+
+    Pageable pageable = PageRequest.of(paging.getPage(),paging.getPageSize());
+    if (query != null) {
+      personPage = personRepo.findByCustomQuery(query, pageable);
+    } else {
+      personPage = personRepo.findAll(pageable);
+    }
     return personPage.getContent();
   }
 
